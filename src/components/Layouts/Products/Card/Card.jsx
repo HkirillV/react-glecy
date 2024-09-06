1f5c815afc89ba431a1143d9dfd94f59210979d7
HKIRILLV <khlebov79@gmail.com>
2024-09-06 13:17:46 +0300

    fix

--- src/components/Layouts/Products/Card/Card.jsx
+++ src/components/Layouts/Products/Card/Card.jsx
@@ -1,4 +1,9 @@
+import {useEffect, useState} from 'react'
+import {v4 as uuidv4} from 'uuid';
+
 import './Card.scss'
+
+const URL = 'http://localhost:3000/cards'
 
 const Card = (props) => {
   const {
@@ -6,9 +11,28 @@ const Card = (props) => {
     children,
   } = props
 
+  const [cards, setCards] = useState([])
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const res = await fetch(URL)
+        const data = await res.json()
+        setCards(data)
+      } catch (err) {
+        console.log(err)
+      }
+    }
+    getData()
+  }, [URL]);
+
   return (
     <div className={className}>
+      {cards.map((item) => (
+        <div className={item} key={item.id}>
 
+        </div>
+      ))}
     </div>
   )
 }
--- src/components/Layouts/Products/Products.jsx
+++ src/components/Layouts/Products/Products.jsx
@@ -1,15 +1,13 @@
 import './Products.scss'
+import Card from './Card'
 
-const Products = (props) => {
-  const {
-
-  } = props
+const Products = () => {
 
   return (
     <div className="products">
         <h4 className="products__title">Попробуйте самые популярные вкусы нашего мороженого</h4>
       <div className="products__items">
-
+          <Card/>
       </div>
     </div>
   )
